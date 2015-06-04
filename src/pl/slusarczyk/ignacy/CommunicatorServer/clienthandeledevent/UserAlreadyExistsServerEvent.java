6d0639626f1558259efeac1de3286088bdc883fd
morzan <ignacy3@gmail.com>
2015-06-04 19:22:05 +0200

    Wszystko skonczone test

--- src/pl/slusarczyk/ignacy/CommunicatorServer/clienthandeledevent/UserAlreadyExistsServerEvent.java
+++
@@ -1,8 +0,0 @@
-package pl.slusarczyk.ignacy.CommunicatorServer.clienthandeledevent;
-
-import java.io.Serializable;
-
-public class UserAlreadyExistsServerEvent extends ClientHandeledEvent implements Serializable
-{
-	private static final long serialVersionUID = 1L;
-}
